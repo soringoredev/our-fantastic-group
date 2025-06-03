52da08be37673f7cd9d5e74cb2882f0bcc367f0f
Harti Alexandrina <hartialexandrina@gmail.com>
2025-06-03 18:38:02 +0200

    Add ex.1

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,2 +1,10 @@
 public class Ex1 {
+    public static void main(String[] args) {
+
+        for ( int i = 0; i < 21; i++){
+            if (i % 2 == 0){
+                System.out.print(i + " ");
+            }
+        }
+    }
 }
